289cdbef1a7dadb0657525fb9292721798d72b05
Nataliya Rieger <artgallery2018@gmx.ch>
2023-09-19 10:50:22 +0200

    Update sendEmail.js

--- helpers/sendEmail.js
+++ helpers/sendEmail.js
@@ -5,7 +5,7 @@ const { META_PASSWORD } = process.env;
 
 const nodemailerConfig = {
   host: "smtp.meta.ua",
-  port: 465, //25, 465, 2525
+  port: 465,
   secure: true,
   auth: {
     user: "inna.pokydko@meta.ua",
@@ -24,37 +24,4 @@ const sendEmail = async (email) => {
     throw error;
   }
 };
-
 module.exports = sendEmail;
-
-
-
-// const nodemailer = require("nodemailer");
-// require("dotenv").config();
-
-// const {META_PASSWORD} = process.env;
-
-// const nodemailerConfig = {
-//     host: "smtp.meta.ua",
-//     port: 465, //25, 465, 2525
-//     secure: true,
-//     auth: {
-//         user: "inna.pokydko@meta.ua",
-//         pass: META_PASSWORD
-//     }
-// };
-
-// const transport = nodemailer.createTransport(nodemailerConfig);
-
-// const email = {
-//     to: "sea@gmail.com",
-//     from: "inna.pokydko@meta.ua",
-//     subject: "Test email",
-//     html: "<p><strong>Test email</strong> from localhost:3000</p>"
-// };
-
-// transport.sendMail(email)
-//     .then(()=> console.log("Email send success"))
-//     .catch(error => console.log(error.message));
-
-  
